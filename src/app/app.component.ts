a2e0b16cc189693d4d05c4bf336e6085a231bc3b
Edoardo Pizzorno <92369419+EdoardoPizzorno@users.noreply.github.com>
2024-03-10 12:59:40 +0100

    Update styles and component templates

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { DataStorageService } from './services/data-storage.service';
+import { AfterViewInit, Component, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   title = 'BrawlStats';
+
+  constructor(private elementRef: ElementRef) { }
+
+  ngAfterViewInit() {
+    this.elementRef.nativeElement.ownerDocument
+      .body.style.backgroundColor = 'var(--secondary-color)';
+  }
 }
